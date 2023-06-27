c6b2c0b35c5329bce7073282ed4ab48ba1d9c5ac
shims94 <shims94@gmail.com>
2023-06-27 10:18:30 +0900

    초기커밋

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import common
+
+common.page_config()
+
+st.title("HealthPoint Locator in Gyeonggi")
+st.caption("""
+HPinGyeunggi는 경기도 소재 의료시설에 대한 정보를 제공하며, 
+경기도 소재 의료시설의 이름과 위치정보, 응급센터등의 특별시설 여부에 대해 정보를 제공하는 프로그램입니다.
+""")
+# st.image("img/welcome.png")
