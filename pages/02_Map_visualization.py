70c2cc688c90321fd6e0e235e188ac4329e3b75f
shims94 <shims94@gmail.com>
2023-06-27 12:39:46 +0900

    의료지원, 외상센터 등등 추가시설에 관한 툴팁 마커에 추가

--- pages/02_Map_visualization.py
+++ pages/02_Map_visualization.py
@@ -3,23 +3,50 @@ from streamlit_folium import st_folium
 import folium
 import common
 
-st.title("Map Visualization")
+# 응급센터가 존재하는지 체크하는 함수
+def check_option(row) :
+    if row['응급의료지원센터여부'] == 'Y': return '응급의료지원센터'
+    elif row['전문응급의료센터여부'] == 'Y': return '전문응급의료센터'
+    elif row['전문응급센터전문분야'] == 'Y': return '전문응급센터전문분야'
+    elif row['권역외상센터여부'] == 'Y': return '권역외상센터'
+    elif row['지역외상센터여부'] == 'Y': return '지역외상센터'
+
 common.page_config()
+
+st.title("Map Visualization")
+st.text("지역급 의료시설은 초록색하트마커로, 권역급 의료시설은 빨간색별마커로 표시")
 
 df = common.get_dataset()
 # 초기 맵
 map_gyeonggi = folium.Map(location=[37.291887, 126.996340], zoom_start=10)
-st_folium(map_gyeonggi)
 
-# 업무 구분명이 지역센터나 기관일경우 녹색 하트마커, 그이외(광역) 빨강 스타마커
-# for index, row in df.iterrows():
-#     if row['업무구분명'] == '지역센터' or row['업무구분명'] == '지역기관':
-#         folium.Marker(location = [row['위도'], row['경도']],
-#                       popup=row['병원또는센터명'],
-#                       icon=folium.Icon(color='green',icon='heart')
-#                      ).add_to(map_gyeonggi)
-#     else:
-#         folium.Marker(location = [row['위도'], row['경도']],
-#                       popup=row['병원또는센터명'],
-#                     icon=folium.Icon(color='yellow',icon='star')
-#                      ).add_to(map_gyeonggi)
+
+#업무 구분명이 지역센터나 기관일경우 녹색 하트마커, 그이외(광역) 빨강 스타마커
+for index, row in df.iterrows():
+    tip = check_option(row)
+    if row['업무구분명'] == '지역센터' or row['업무구분명'] == '지역기관':
+        if tip != '':
+            folium.Marker(location=[row['위도'], row['경도']],
+                          popup=row['병원명/센터명'],
+                          tooltip=tip,
+                          icon=folium.Icon(color='green', icon='heart')
+                          ).add_to(map_gyeonggi)
+        else:
+            folium.Marker(location = [row['위도'], row['경도']],
+                          popup=row['병원명/센터명'],
+                          icon=folium.Icon(color='green',icon='heart')
+                         ).add_to(map_gyeonggi)
+    else:
+        if tip != '':
+            folium.Marker(location=[row['위도'], row['경도']],
+                          popup=row['병원명/센터명'],
+                          tooltip=tip,
+                          icon=folium.Icon(color='yellow', icon='star')
+                          ).add_to(map_gyeonggi)
+        else :
+            folium.Marker(location = [row['위도'], row['경도']],
+                      popup=row['병원명/센터명'],
+                    icon=folium.Icon(color='yellow',icon='star')
+                     ).add_to(map_gyeonggi)
+
+st_folium(map_gyeonggi)
